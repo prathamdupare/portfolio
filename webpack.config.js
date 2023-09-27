840d249e7cff3ffe176817f2477410f529379318
prathamdupare <prathamdupare@proton.me>
2023-09-27 16:45:49 +0530

    Changed path  of css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+// webpack.config.js
+
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.js", // The entry point of your application
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js", // The name of the output bundle
+    path: path.resolve(__dirname, "dist"), // The output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Match JavaScript files
+        exclude: /node_modules/, // Exclude node_modules
+        use: {
+          loader: "babel-loader", // Use Babel for JavaScript transpilation
+        },
+      },
+      {
+        test: /\.css$/, // Match CSS files
+        use: ["style-loader", "css-loader"], // Use style-loader and css-loader
+      },
+    ],
   },
 };
